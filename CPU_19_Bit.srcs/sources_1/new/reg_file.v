2da26c767b385cd2e9f2741b5c0e16cb927d374b
Suryadev Singh <157358475+SuryaIIITG@users.noreply.github.com>
2024-12-10 14:14:47 +0530

    Update reg_file.v

--- CPU_19_Bit.srcs/sources_1/new/reg_file.v
+++ CPU_19_Bit.srcs/sources_1/new/reg_file.v
@@ -26,16 +26,20 @@ input reset,
 input [4:0] rs,
 input [4:0] rd,
 input [18:0] alu_result,
+input write_enable,
 output reg [18:0] reg_data1,
 output reg [18:0] reg_data2
     );
     reg [18:0] register [0:31];
+    integer i;
     always @(posedge clk or posedge reset) begin
     if (reset) begin
     // Reset all regidters
-    register[0] <= 0;
-    end else begin
-    register[rd] <= alu_result; 
+    for (i = 0; i < 32; i = i + 1)
+        register[i] <= 19'b0;
+    end else if (write_enable && rd != 0)
+    begin
+    register[rd] <= alu_result;
     end
     end
     always @(posedge clk) begin
